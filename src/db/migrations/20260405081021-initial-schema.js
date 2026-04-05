3f9e9ffb2fb7f29c78db425c94a11d0c2e52ede1
Robenildo Noberto <92067375+RobenildoN@users.noreply.github.com>
2026-04-05 05:14:51 -0300

    refactor: Ajustar formatação das funções de migração para consistência

--- src/db/migrations/20260405081021-initial-schema.js
+++ src/db/migrations/20260405081021-initial-schema.js
@@ -2,7 +2,7 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     // Criar tabela categories
     await queryInterface.createTable('categories', {
       id: {
@@ -94,7 +94,7 @@ module.exports = {
     });
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     // Remover tabelas na ordem reversa
     await queryInterface.dropTable('posts');
     await queryInterface.dropTable('categories');
